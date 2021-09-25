f303534037b36f499875c9725ca1ecb8cba08b7d
anxious gritt <roshanjalil609@gmail.com>
2021-09-25 22:47:46 +0500

    set up template

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height:{
+        "h-100":"45rem",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
